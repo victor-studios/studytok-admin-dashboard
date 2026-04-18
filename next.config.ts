2fdec2413dfc1f9e0ba95dba43c2ff0174423ab6
72salman <salmanali@gmail.com>
2026-04-18 16:54:38 +0500

    Update Next.js to 16.2.4 to fix Vercel security block

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  /* config options here */
 };
 
 export default nextConfig;
